09b8ee1924cb734b7619f1d1f783720ab7782c9a
Robin Stumm <serverkorken@gmail.com>
2025-01-24 21:04:56 +0100

    expose `.#shellModules.zig` and provide only the `default` dev shell

--- nix/devShells.nix
+++ nix/devShells.nix
@@ -1,23 +1,21 @@
-{inputs, ...}: {
+{
+  inputs,
+  config,
+  ...
+}: {
   imports = with inputs; [
     make-shell.flakeModules.default
   ];
 
-  perSystem = {
-    config,
-    pkgs,
-    ...
-  }: {
-    devShells.default = config.make-shells.zig.finalPackage;
+  flake.shellModules.zig = {pkgs, ...}: {
+    packages = with pkgs; [zig zls];
 
-    make-shells.zig = {
-      packages = with pkgs; [zig zls];
-
-      shellHook = ''
-        # TODO remove once merged: https://github.com/NixOS/nixpkgs/pull/310588
-        # Set to `/build/tmp.XXXXXXXXXX` by the zig hook.
-        unset ZIG_GLOBAL_CACHE_DIR
-      '';
-    };
+    shellHook = ''
+      # TODO remove once merged: https://github.com/NixOS/nixpkgs/pull/310588
+      # Set to `/build/tmp.XXXXXXXXXX` by the zig hook.
+      unset ZIG_GLOBAL_CACHE_DIR
+    '';
   };
+
+  perSystem.make-shells.default.imports = [config.flake.shellModules.zig];
 }
