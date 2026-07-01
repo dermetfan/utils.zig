5aa0cc10e64c4045515bb856b12b78efb2030744
Robin Stumm <serverkorken@gmail.com>
2026-07-01 23:13:35 +0200

    base `buildZigPackage` on `lib.extendMkDerivation`

--- nix/overlays/zig/default.nix
+++ nix/overlays/zig/default.nix
@@ -4,13 +4,16 @@
   ...
 }: {
   flake.overlays.zig = final: prev: {
-    buildZigPackage = lib.makeOverridable (prev.callPackage (
-      {
-        lib,
-        stdenv,
-        runCommand,
-        zig,
-      }: args @ {
+    buildZigPackage = lib.extendMkDerivation {
+      constructDrv = final.stdenv.mkDerivation;
+      excludeDrvArgNames = [
+        "buildZigZon"
+        "zigDepsHash"
+        "zigRelease"
+        "zigTarget"
+        "zigDynamicLinker"
+      ];
+      extendDrvArgs = finalAttrs: args @ {
         src,
         buildZigZon ? "build.zig.zon",
         zigDepsHash ? "",
@@ -18,163 +21,154 @@
         zigRelease ? true,
         # Passed to `-Dtarget` if not null.
         zigTarget ? let
-          zig-env = inputs.zig2nix.zig-env.${stdenv.hostPlatform.system} {inherit (final) zig;};
+          zig-env = inputs.zig2nix.zig-env.${final.stdenv.hostPlatform.system} {inherit (final) zig;};
         in
-          (zig-env.target stdenv.targetPlatform.system).zig,
+          (zig-env.target final.stdenv.targetPlatform.system).zig,
         # Passed to `-Ddynamic-linker` if not null.
         zigDynamicLinker ?
           if zigTarget == null
           then null
-          else stdenv.cc.bintools.dynamicLinker,
+          else final.stdenv.cc.bintools.dynamicLinker,
         ...
-      }:
-        stdenv.mkDerivation (
-          finalAttrs: let
-            info = lib.importJSON (finalAttrs.passthru.packageInfo.overrideAttrs {
-              withFingerprint = false;
-            });
-          in
-            {
-              pname = info.name;
-              inherit (info) version;
+      }: let
+        info = lib.importJSON (finalAttrs.passthru.packageInfo.overrideAttrs {
+          withFingerprint = false;
+        });
+      in {
+        pname = info.name;
+        inherit (info) version;
 
-              postPatch = lib.optionalString (finalAttrs.passthru ? deps) ''
-                cd ${lib.escapeShellArg (builtins.dirOf buildZigZon)}
-              '';
+        postPatch = lib.optionalString (finalAttrs.passthru ? deps) ''
+          cd ${lib.escapeShellArg (builtins.dirOf buildZigZon)}
+        '';
 
-              doCheck = true;
+        doCheck = true;
 
-              dontStrip =
-                if builtins.isBool zigRelease
-                then !zigRelease
-                else zigRelease == "Debug";
-            }
-            // builtins.removeAttrs args [
-              "buildZigZon"
-              "zigDepsHash"
-            ]
-            // {
-              nativeBuildInputs =
-                args.nativeBuildInputs or []
-                ++ lib.singleton zig.hook;
+        dontStrip =
+          if builtins.isBool zigRelease
+          then !zigRelease
+          else zigRelease == "Debug";
 
-              env =
-                args.env or {}
-                // rec {
-                  zig_default_optimize_flag =
-                    if builtins.isBool zigRelease
-                    then "-Drelease=${builtins.toJSON zigRelease}"
-                    else "-Doptimize=${zigRelease}";
+        nativeBuildInputs =
+          args.nativeBuildInputs or []
+          ++ lib.singleton final.zig.hook;
 
-                  zigBuildFlags = toString (
-                    [
-                      "--system"
-                      finalAttrs.passthru.deps
+        env =
+          args.env or {}
+          // rec {
+            zig_default_optimize_flag =
+              if builtins.isBool zigRelease
+              then "-Drelease=${builtins.toJSON zigRelease}"
+              else "-Doptimize=${zigRelease}";
 
-                      "-freference-trace"
-                    ]
-                    ++ lib.optional (zigTarget != null) "-Dtarget=${zigTarget}"
-                    ++ lib.optional (zigDynamicLinker != null) "-Ddynamic-linker=${zigDynamicLinker}"
-                  );
+            zigBuildFlags = toString (
+              [
+                "--system"
+                finalAttrs.passthru.deps
 
-                  zigCheckFlags = zigBuildFlags;
-                };
+                "-freference-trace"
+              ]
+              ++ lib.optional (zigTarget != null) "-Dtarget=${zigTarget}"
+              ++ lib.optional (zigDynamicLinker != null) "-Ddynamic-linker=${zigDynamicLinker}"
+            );
 
-              passthru =
-                {
-                  packageInfo = final.zigPackageInfo (
-                    lib.optionalString (!lib.hasPrefix "/" buildZigZon) "${finalAttrs.src}/"
-                    + buildZigZon
-                  );
+            zigCheckFlags = zigBuildFlags;
+          };
 
-                  # builds the $ZIG_GLOBAL_CACHE_DIR/p directory
-                  depsCache = let
-                    buildZigZonDir = builtins.dirOf buildZigZon;
-                  in
-                    runCommand (with finalAttrs; "${pname}-${version}-deps-cache") {
-                      nativeBuildInputs = [zig];
+        passthru =
+          args.passthru or {}
+          // {
+            packageInfo = final.zigPackageInfo (
+              lib.optionalString (!lib.hasPrefix "/" buildZigZon) "${finalAttrs.src}/"
+              + buildZigZon
+            );
 
-                      outputHashMode = "recursive";
-                      outputHashAlgo = "sha256";
-                      outputHash = zigDepsHash;
-                    } (
-                      ''
-                        mkdir "$TMPDIR"/{src,cache}
+            # builds the $ZIG_GLOBAL_CACHE_DIR/p directory
+            depsCache = let
+              buildZigZonDir = builtins.dirOf buildZigZon;
+            in
+              final.runCommand (with finalAttrs; "${pname}-${version}-deps-cache") {
+                nativeBuildInputs = [final.zig];
 
-                        shopt -s nullglob
-                        cp --recursive --symbolic-link \
-                          ${src}/* \
-                          ${src}/.* \
-                          "$TMPDIR"/src/
-                        cd "$TMPDIR"/src
-                      ''
-                      + (
-                        lib.foldl
-                        (
-                          acc: component: let
-                            path = lib.path.subpath.join [acc.path component];
-                          in
-                            acc
-                            // {
-                              inherit path;
-                              script =
-                                acc.script
-                                + "\n"
-                                + ''
-                                  chmod --recursive u+w ${lib.escapeShellArg component}
-                                  rm --recursive ${lib.escapeShellArg component}
-                                  mkdir ${lib.escapeShellArg component}
-                                  cp --recursive --symbolic-link \
-                                    ${src}/${lib.escapeShellArg path}/* \
-                                    ${src}/${lib.escapeShellArg path}/.* \
-                                    ${lib.escapeShellArg component}
-                                  cd ${lib.escapeShellArg component}
-                                '';
-                            }
-                        )
-                        {
-                          path = ".";
-                          script = "";
-                        }
-                        (lib.path.subpath.components buildZigZonDir)
-                      )
-                        .script
-                      + ''
-                        cd "$TMPDIR"/src/${lib.escapeShellArg buildZigZonDir}
+                outputHashMode = "recursive";
+                outputHashAlgo = "sha256";
+                outputHash = zigDepsHash;
+              } (
+                ''
+                  mkdir "$TMPDIR"/{src,cache}
 
-                        # `zig build --fetch` does not fetch lazy dependencies
-                        # so we make them all eager to make sure they are all present.
-                        # XXX Of course this means that we fetch all dependencies
-                        # even if the build does not actually need them.
-                        # see https://github.com/ziglang/zig/issues/20976
-                        rm build.zig.zon
-                        sed 's/\.lazy[[:space:]]*=[[:space:]]*true[[:space:]]*,\?//' > build.zig.zon \
-                          ${src}/${lib.escapeShellArg buildZigZon}
+                  shopt -s nullglob
+                  cp --recursive --symbolic-link \
+                    ${src}/* \
+                    ${src}/.* \
+                    "$TMPDIR"/src/
+                  cd "$TMPDIR"/src
+                ''
+                + (
+                  lib.foldl
+                  (
+                    acc: component: let
+                      path = lib.path.subpath.join [acc.path component];
+                    in
+                      acc
+                      // {
+                        inherit path;
+                        script =
+                          acc.script
+                          + "\n"
+                          + ''
+                            chmod --recursive u+w ${lib.escapeShellArg component}
+                            rm --recursive ${lib.escapeShellArg component}
+                            mkdir ${lib.escapeShellArg component}
+                            cp --recursive --symbolic-link \
+                              ${src}/${lib.escapeShellArg path}/* \
+                              ${src}/${lib.escapeShellArg path}/.* \
+                              ${lib.escapeShellArg component}
+                            cd ${lib.escapeShellArg component}
+                          '';
+                      }
+                  )
+                  {
+                    path = ".";
+                    script = "";
+                  }
+                  (lib.path.subpath.components buildZigZonDir)
+                )
+                    .script
+                + ''
+                  cd "$TMPDIR"/src/${lib.escapeShellArg buildZigZonDir}
 
-                        zig build --fetch \
-                          --cache-dir "$TMPDIR" \
-                          --global-cache-dir "$TMPDIR"/cache
+                  # `zig build --fetch` does not fetch lazy dependencies
+                  # so we make them all eager to make sure they are all present.
+                  # XXX Of course this means that we fetch all dependencies
+                  # even if the build does not actually need them.
+                  # see https://github.com/ziglang/zig/issues/20976
+                  rm build.zig.zon
+                  sed 's/\.lazy[[:space:]]*=[[:space:]]*true[[:space:]]*,\?//' > build.zig.zon \
+                    ${src}/${lib.escapeShellArg buildZigZon}
 
-                        # create an empty directory if there are no dependencies
-                        mv "$TMPDIR"/cache/p $out || mkdir $out
-                      ''
-                    );
+                  zig build --fetch \
+                    --cache-dir "$TMPDIR" \
+                    --global-cache-dir "$TMPDIR"/cache
 
-                  # builds the zig-pkg directory
-                  # newer zig versions can consume this directly using --system
-                  deps = runCommand (with finalAttrs; "${pname}-${version}-deps") {} ''
-                    mkdir "$out"
-                    for tarball in ${finalAttrs.passthru.depsCache}/*; do
-                      name=$(basename "$tarball" .tar.gz)
-                      mkdir "$out/$name"
-                      tar --extract --file "$tarball" --directory "$out"
-                    done
-                  '';
-                }
-                // args.passthru or {};
-            }
-        )
-    ) {});
+                  # create an empty directory if there are no dependencies
+                  mv "$TMPDIR"/cache/p $out || mkdir $out
+                ''
+              );
+
+            # builds the zig-pkg directory
+            # newer zig versions can consume this directly using --system
+            deps = final.runCommand (with finalAttrs; "${pname}-${version}-deps") {} ''
+              mkdir "$out"
+              for tarball in ${finalAttrs.passthru.depsCache}/*; do
+                name=$(basename "$tarball" .tar.gz)
+                mkdir "$out/$name"
+                tar --extract --file "$tarball" --directory "$out"
+              done
+            '';
+          };
+      };
+    };
 
     zigPackageInfo = buildZigZon:
       prev.runCommand "zig-package-info" rec {
